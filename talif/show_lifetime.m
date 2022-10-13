2d1ceda19fbbc12d7ea1efb557bb97a1b44ce12c
Jan Singon Slany <singond@seznam.cz>
2022-10-13 10:40:12 +0200

    talif: Add visualisation script

---
+++ talif/show_lifetime.m
@@ -0,0 +1,15 @@
+if (!exist("X"))
+	lifetime;
+end
+
+figure;
+title("Time evolution of intensity");
+xlabel("time t[ns]");
+ylabel("intensity [a.u.]");
+hold on;
+for x = X
+	plot(x.t, x.in ./ max(x.in),
+		"displayname", sprintf("%d Pa", x.p1));
+end
+hold off;
+legend location northeast;
