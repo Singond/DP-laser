3dd32d3dae05b5c51dc8c74ddb6e67a8c2e2b49c
Jan Singon Slany <singond@seznam.cz>
2022-12-13 10:18:35 +0100

    text: Add efish electric field results

--- efish/export.jl
+++ efish/export.jl
@@ -20,3 +20,22 @@ end
 E1 = LinRange(minimum(E), maximum(E), 100)
 @gp :- E1./1e3 calib(E1) "w l ls 1 dt 2 t 'model \$\\efish \\sim \\elfield^2\$'" :-
 save(term="epslatex size 12cm,8cm", output="plots/calib.tex")
+
+include("main.jl")
+@gp """
+	load '../style.gp'
+	set xyplane at 0
+	set xlabel '\$\\tim\\,[\\si{\\micro\\second}]\$' offset 0,-1
+	set xtics offset 0,-0.5
+	set ylabel '\$y\\,[\\si{\\milli\\metre}]\$' offset 0,-1
+	set zlabel '\$\\elfield\\,[\\si{\\kilo\\volt\\per\\metre}]\$' rotate by 90
+	unset key
+"""
+for k in [8:-1:1; 10:13]
+	x = X[k]
+	t = x.t .- x.t[1]
+	yy = ones(size(x.t)) * y[k]
+	zz = zeros(size(x.t))
+	@gsp :- t yy x.Iefish zz x.Iefish "w zerrorfill fc 'white' ls 1" :-
+end
+save(term="epslatex size 12cm,8cm", output="plots/period-elfield.tex")
