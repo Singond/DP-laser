64e1bdff71c1b36be44e8dfb3ad09ec3bb488505
Jan Slany <jan.slany@mail.muni.cz>
2023-02-17 16:57:15 +0100

    lif: Evaluate all wavelength data

--- lif/wavelen.m
+++ lif/wavelen.m
@@ -5,7 +5,7 @@ if (isfolder("../octave-local"))
 	addpath ../octave-local;
 end
 
-amp = [50 40 30 20 10 50 40 30];
+amp = [50 40 30 20 13 50 40 30 24 15];
 wl = (195.95:0.001:196.2)';
 
 D = struct();
@@ -15,8 +15,11 @@ D(k++) = load_iccd("data-2023-01-20/scan5.SPE");
 D(k++) = load_iccd("data-2023-01-20/scan6.SPE");
 D(k++) = load_iccd("data-2023-01-20/scan7.SPE");
 D(k++) = load_iccd("data-2023-01-20/scan8.SPE");
-D(k++) = load_iccd("data-2023-01-20/scan9.SPE");
+D(k++) = load_iccd("data-2023-01-20/scan9.SPE");   # First set with spatial f.
 D(k++) = load_iccd("data-2023-01-20/scan10.SPE");
+D(k++) = load_iccd("data-2023-01-20/scan11.SPE");
+D(k++) = load_iccd("data-2023-01-20/scan12.SPE");
+D(k++) = load_iccd("data-2023-01-20/scan13.SPE");  # Swapped powermeters
 
 X = struct();
 for k = 1:length(D);
