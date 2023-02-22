c2919c2cdf400363bffa0a164c5f46c09180c13e
Jan Singon Slany <singond@seznam.cz>
2023-02-23 00:06:51 +0100

    lif: Evaluate saturation from newer data set

--- lif/saturation.m
+++ lif/saturation.m
@@ -13,20 +13,13 @@ k = 1;
 #X(k++) = load_iccd("data-2023-01-18/saturation4.SPE");
 #X(k++) = load_iccd("data-2023-01-18/saturation5.SPE");
 #X(k++) = load_iccd("data-2023-01-18/saturation6.SPE");
-X(k++) = load_iccd("data-2023-01-18/saturation7.SPE");
-X(k++) = load_iccd("data-2023-01-18/saturation8.SPE");
-X(k++) = load_iccd("data-2023-01-18/saturation9.SPE");
+#X(k++) = load_iccd("data-2023-01-18/saturation7.SPE");
+#X(k++) = load_iccd("data-2023-01-18/saturation8.SPE");
+#X(k++) = load_iccd("data-2023-01-18/saturation9.SPE");
+X(k++) = load_iccd("data-2023-01-20/saturace1.SPE");
+X(k++) = load_iccd("data-2023-01-20/saturace2.SPE");
 
-function x = process(x)
-	x.inraw = squeeze(sum(sum(x.img, 1), 2));
-	x.in = x.inraw ./ (x.acc);
-end
-
-## TODO: Change camera and powermeter delay to 0, because the
-## synchronization is now done precisely by computer during measurement.
-## (Also in previous TALIF measurement in January.)
-#m = [4:5];
-#X(m) = arrayfun(@(x) crop_iccd(x, [140 180], [461 504]), X(m));
 X = arrayfun(@correct_iccd, X);
 X = arrayfun(@(x) frametimes(x, 50), X);
+X = arrayfun(@img_intensity, X);
 X = arrayfun(@process, X);
