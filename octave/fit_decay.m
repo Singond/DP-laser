326abb5b782180c39ad7071dd6f4ccb33bafd8b0
Jan Singon Slany <singond@seznam.cz>
2023-02-23 23:18:48 +0100

    Refactor fit_decay

--- octave/fit_decay.m
+++ octave/fit_decay.m
@@ -9,10 +9,12 @@ function x = fit_decay(x)
 	t0 = t(1);
 	in = x.in(pk:end);
 
+	## Linearized model (preliminary fit)
 	x.fitl.beta = polyfit(t, log(in), 1);
 	x.fitl.tau = -1 / x.fitl.beta(1);
 	x.fitl.f = @(t) exp(polyval(x.fitl.beta, t));
 
+	## Exponential model
 	model = @(t, b) b(1) .* exp(-t./b(2));
 	b0 = [in(1), x.fitl.tau];
 	s = struct();
@@ -24,14 +26,15 @@ function x = fit_decay(x)
 	s.tau = s.beta(2);
 	x.fite = s;
 
-	model = @(t, b) b(1) .* exp(-t./b(2)) + b(3);
+	## Exponential model with constant
+	modelb = @(t, b) b(1) .* exp(-t./b(2)) + b(3);
 	b0 = [in(1), x.fite.tau, 0];
 	s = struct();
-	[~, s.beta, s.cvg, s.iter] = leasqr(t - t0, in, b0, model, [], 30);
+	[~, s.beta, s.cvg, s.iter] = leasqr(t - t0, in, b0, modelb, [], 30);
 	if (!s.cvg)
 		warning("Convergence not reached after %d iterations.", s.iter);
 	end
-	s.f = @(t) model(t - t0, s.beta);
+	s.f = @(t) modelb(t - t0, s.beta);
 	s.tau = s.beta(2);
 	s.bg = s.beta(3);
 	x.fitb = s;
