ae8a4a3d77388e3d310070cbc095e2e0f842288a
Jan Singon Slany <singond@seznam.cz>
2023-03-20 03:05:12 +0100

    Support image mask in img_intensity

--- octave/img_intensity.m
+++ octave/img_intensity.m
@@ -1,4 +1,29 @@
-function x = img_intensity(x)
-	x.inraw = squeeze(sum(sum(x.img, 1), 2));
-	x.in = x.inraw ./ (x.acc);
+## -*- texinfo -*-
+## @deftypefn  {} {@var{x} =} img_intensity (@var{x})
+## @deftypefnx {} {@var{x} =} img_intensity (@var{x}, @var{mask})
+## @end deftypefn
+function r = img_intensity(x, mask = [])
+	if (isstruct(x))
+		img = x.img;
+		acc = x.acc;
+	elseif (isnumeric(x))
+		img = x;
+		wt = 1;
+	else
+		error("img_intensity: X must be a numeric array or a struct");
+	end
+
+	if (!isempty(mask))
+		img = img .* mask;
+	end
+
+	in = squeeze(sum(sum(img, 1), 2));
+
+	if (isstruct(x))
+		r = x;
+		r.inraw = in;
+		r.in = in ./ acc;
+	else
+		r = in;
+	end
 end
