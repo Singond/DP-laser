352d0fbcbe12dcf01783f5b39b9fdb949c198de7
Jan Singon Slany <singond@seznam.cz>
2023-03-20 21:05:50 +0100

    text: Add lifetime plot

---
+++ lif/lifetime_export.m
@@ -0,0 +1,25 @@
+pkg load report;
+
+lifetime;
+
+gp = gnuplotter;
+gp.load("../style-cairo.gp");
+gp.exec("set palette gray");
+gp.title('Časový vývoj');
+gp.xlabel('čas $\\tim\\,[\\si{\\nano\\second}]$');
+gp.ylabel('intenzita $\\ity\\,[\\si\\arbunit]$');
+gp.exec("\n\
+	set xrange [4:16] \n\
+	set yrange [0:100] \n\
+	set key samplen 2 height 1 \n\
+");
+tt = linspace(min(X(2).t), max(X(2).t), 1000);
+gp.plot(X(2).t*1e9, X(2).in_center, "w p ls 1 t 'oblast 1'");
+gp.plot(tt*1e9, X(2).fit_center.fite.f(tt), "w l ls 1 dt 2 t ''");
+gp.plot(X(2).t*1e9, X(2).in_edge1,  "w p ls 2 t 'oblast 2'");
+gp.plot(tt*1e9, X(2).fit_edge1.fite.f(tt),  "w l ls 2 dt 3 t ''");
+gp.plot(X(2).t*1e9, X(2).in_edge2,  "w p ls 3 t 'oblast 3'");
+gp.plot(tt*1e9, X(2).fit_edge2.fite.f(tt),  "w l ls 3 dt 4 t ''");
+gp.export("results/lifetime.tex", "cairolatex", "size 8cm,6cm");
+gp.export("results/lifetime-lg.tex", "cairolatex", "size 10cm,8cm");
+clear gp;
---
+++ style-cairo.gp
@@ -0,0 +1,8 @@
+set style line 1 lw 2 lc rgb '#377EB8' pt 5  ps 0.7  # blue
+set style line 2 lw 2 lc rgb '#E41A1C' pt 13 ps 1    # red
+set style line 3 lw 2 lc rgb '#4DAF4A' pt 9  ps 1    # green
+set style line 4 lw 2 lc rgb '#984EA3' pt 11 ps 1    # purple
+set style line 5 lw 2 lc rgb '#FF7F00' pt 7  ps 0.75 # orange
+set style line 6 lw 2 lc rgb '#DDDD33' pt 15 ps 1    # yellow
+set style line 7 lw 2 lc rgb '#A65628' pt 3  ps 0.65 # brown
+set style line 8 lw 2 lc rgb '#F781BF' pt 9  ps 1    # pink
