074ae57ac42fd3cb39fa305fce683cf0b1e2453c
Jan Singon Slany <singond@seznam.cz>
2023-03-21 23:26:40 +0100

    Add tests to img_intensity

--- octave/img_intensity.m
+++ octave/img_intensity.m
@@ -30,3 +30,54 @@ function r = img_intensity(x, mask = [])
 		r = in;
 	end
 end
+
+%!shared img, mask1, mask2
+%! img = [
+%!   6 7 5 9 1
+%!   2 4 3 8 2
+%!   0 9 2 8 4
+%!   1 3 6 7 1
+%!   6 2 0 7 9
+%! ];
+
+%!assert(img_intensity(img), mean(img(:)));
+
+%!test
+%! x.img = img;
+%! x.acc = 2;
+%! x = img_intensity(x);
+%! assert(x.inraw, mean(img(:)));
+%! assert(x.in, mean(img(:)) / 2);
+
+%! mask1 = [
+%!   0 0 0 0 0
+%!   0 1 1 1 0
+%!   0 1 1 1 0
+%!   0 1 1 1 0
+%!   0 0 0 0 0
+%! ];
+%! mask2 = [
+%!   1 1 1 1 0
+%!   1 0 0 0 1
+%!   1 1 0 1 0
+%!   0 0 0 1 1
+%!   1 0 1 0 1
+%! ];
+
+%!assert(img_intensity(img, mask1), mean(img(2:4,2:4)(:)));
+
+%!test
+%! x.img = img;
+%! x.acc = 2;
+%! x = img_intensity(x, mask1);
+%! assert(x.inraw, mean(img(2:4,2:4)(:)));
+%! assert(x.in, mean(img(2:4,2:4)(:)) / 2);
+
+%!assert(img_intensity(img, mask2), mean(img(logical(mask2))));
+
+%!test
+%! x.img = img;
+%! x.acc = 2;
+%! x = img_intensity(x, mask2);
+%! assert(x.inraw, mean(img(logical(mask2))));
+%! assert(x.in, mean(img(logical(mask2))) / 2);
