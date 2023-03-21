fd632935b9b0d1baaeed8a23809d7727b3dbe8d3
Jan Singon Slany <singond@seznam.cz>
2023-03-21 01:15:03 +0100

    text: Add excitation profile plots

---
+++ lif/wavelen_export.m
@@ -0,0 +1,42 @@
+pkg load report;
+
+wavelen;
+
+gp = gnuplotter;
+gp.load("../style-cairo.gp");
+gp.title("Excitační profil (bez~prostorového filtru)");
+gp.xlabel('vlnová délka laseru $\\enlaser\\,[\\si{\\nano\\metre}]$');
+gp.ylabel('intenzita $\\itylif\\,[\\si\\arbunit]$');
+gp.exec("\n\
+	set xrange [195.95:196.20] \n\
+	set key top right samplen 2 height 1 \n\
+");
+k = 1;
+for x = 1:5
+	x = X(k);
+	Em = mean(x.E(:,end));
+	gp.plot(x.wl, x.in, sprintf(
+		"w l ls %d t '$\\SI{%.0f}{\\micro\\joule}$'", k, Em*1e6));
+	k++;
+end
+gp.export("results/excitprof-nofilter-lg.tex", "cairolatex", "size 10cm,8cm");
+clear gp k;
+
+gp = gnuplotter;
+gp.load("../style-cairo.gp");
+gp.title("Excitační profil (s~prostorovým filtrem)");
+gp.xlabel('vlnová délka laseru $\\enlaser\\,[\\si{\\nano\\metre}]$');
+gp.ylabel('intenzita $\\itylif\\,[\\si\\arbunit]$');
+gp.exec("\n\
+	set xrange [195.95:196.20] \n\
+	set key top right samplen 2 height 1 \n\
+");
+k = 1;
+for x = X(6:10)
+	Em = mean(x.E(:,end));
+	gp.plot(x.wl, x.in, sprintf(
+		"w l ls %d t '$\\SI{%.2f}{\\micro\\joule}$'", k, Em*1e6));
+	k++;
+end
+gp.export("results/excitprof-filter-lg.tex", "cairolatex", "size 10cm,8cm");
+clear gp k;
