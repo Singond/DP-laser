71844b04b199359c075217e1926e3fa1a8c9de62
Jan Singon Slany <singond@seznam.cz>
2023-10-05 00:09:22 +0200

    lif: Ensure alignment between image and fit

--- lif/lifetime_full.m
+++ lif/lifetime_full.m
@@ -1,6 +1,10 @@
 lifetime_base;
 
 x = lifetime(2);
-in = x.img ./ x.acc;
-fits = fit_decay(x.t, in(60:70,:,:), "dim", 3, "from", 7e-9, "progress");
-tau = cellfun(@(c) c.fite.tau, fits);
+x.in = x.img ./ x.acc;
+aoi = {60:90, 50:150};  # Area of interest
+fits = struct();
+fits(aoi{:}) = fit_decay(x.t, x.in(aoi{:},:),
+	"dim", 3, "from", 7e-9, "progress");
+tau = NA(size(x.in)(1:2));
+tau(aoi{:}) = arrayfun(@(a) a.fite.tau, fits(aoi{:}));
