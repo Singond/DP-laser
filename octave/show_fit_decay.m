3bdbe75295a5a2a8dc494845344445aea9510bfe
Jan Slany <jan.slany@mail.muni.cz>
2023-10-31 13:05:03 +0100

    lif: Make interactive plot compatible with older versions

--- octave/show_fit_decay.m
+++ octave/show_fit_decay.m
@@ -34,5 +34,12 @@ function show_fit_decay(s, fit, r, c, field="fite")
 			"--", "color", cc, "handlevisibility", "off");
 	xlabel("time t [ns]");
 	ylabel("inensity I [a.u.]");
-	legend("interpreter", "tex");
+
+	## Compatibility hack for older Octave versions (tested on 5.2.0):
+	## Set location and orientation explicitly to "default" to force
+	## an update of the legend from current "displayname" properties.
+	## This is not necessary in newer versions, where the legend is
+	## updated automatically.
+	hleg = legend("location", "default", "orientation", "default");
+	set(hleg, "interpreter", "tex");
 end
