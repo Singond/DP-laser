db80d8530d2906f2bb58479bbffb06fbdf51b06a
Jan Singon Slany <singond@seznam.cz>
2023-11-01 22:21:26 +0100

    Enable selecting fits in show_fit_decay

--- octave/show_fit_decay.m
+++ octave/show_fit_decay.m
@@ -1,10 +1,16 @@
 ## -*- texinfo -*-
 ## @deftypefn  {} {} show_fit_decay (@var{s}, @var{fit}, @var{r}, @var{c})
-## @deftypefnx {} {} show_fit_decay (@dots{}, @var{field})
+## @deftypefnx {} {} show_fit_decay (@dots{}, @code{@{"fitl", @dots{}@}})
 ##
 ## Inspect the fit produced by @code{fit_decay}.
+##
+## The optional cell array argument selects which fits in @var{fit}
+## will be plotted. The default is to plot all fits.
 ## @end deftypefn
-function show_fit_decay(s, fit, r, c, field="fite")
+function show_fit_decay(s, fit, r, c, fields={})
+	if (isempty(fields))
+		fields = {"fitl", "fitb", "fite"};
+	end
 	x = s.t;
 	y = s.in(r,c,:);
 
@@ -12,18 +18,28 @@ function show_fit_decay(s, fit, r, c, field="fite")
 	[ymin, ymax] = bounds(y);
 
 	xx = linspace(xmin, xmax);
-	yl = fit(r,c).fitl.f(xx);    # linear fit in log scale
-	ye = fit(r,c).fite.f(xx);    # exponential fit
+	yl = yb = ye = [];
+	if (any(strcmp(fields, "fitl")))
+		yl = fit(r,c).fitl.f(xx);    # linear fit in log scale
+	end
+	if (any(strcmp(fields, "fitb")))
+		yb = fit(r,c).fitb.f(xx);    # exponential fit with y-constant
+	end
+	if (any(strcmp(fields, "fite")))
+		ye = fit(r,c).fite.f(xx);    # exponential fit
+	end
 	taue = fit(r,c).fite.tau;
 
 	## Limit fitted functions to similar y-range as data
 	if (strcmp(get(gca, "yscale"), "log"))
 		ymin = min(y(y > 0));
 		ml = (ymin * 0.1) < yl & yl < (ymax * 10);
+		mb = (ymin * 0.1) < yb & yb < (ymax * 10);
 		me = (ymin * 0.1) < ye & ye < (ymax * 10);
 	else
 		yrange = ymax - ymin;
 		ml = (ymin - 0.1 * yrange) < yl & yl < (ymax + 0.1 * yrange);
+		mb = (ymin - 0.1 * yrange) < yb & yb < (ymax + 0.1 * yrange);
 		me = (ymin - 0.1 * yrange) < ye & ye < (ymax + 0.1 * yrange);
 	end
 
@@ -31,13 +47,15 @@ function show_fit_decay(s, fit, r, c, field="fite")
 	cc = get(gca(), "colororder")(cidx,:);
 	xscale = 1e9;
 	plot(
-		x * xscale, y, "bd",
+		x * xscale, y, "d",
 			"color", cc, "displayname",
 			sprintf("[%d, %d] \\tau_e = %.3f ns", r, c, taue * xscale),
 		xx(ml) * xscale, yl(ml),
 			"b:", "color", cc, "handlevisibility", "off",
+		xx(mb) * xscale, yb(mb),
+			"b-.", "color", cc, "handlevisibility", "off",
 		xx(me) * xscale, ye(me),
-			"--", "color", cc, "handlevisibility", "off");
+			"b--", "color", cc, "handlevisibility", "off");
 	xlabel("time t [ns]");
 	ylabel("inensity I [a.u.]");
 
