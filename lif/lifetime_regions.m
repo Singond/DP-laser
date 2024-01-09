8a68c4ece56b46143fbda74eec9c9d064177e9e0
Jan Slany <jan.slany@mail.muni.cz>
2024-01-09 16:20:50 +0100

    lif: Fix lifetime fits failing after redefining fit_decay
    After redefining the fit_decay function, some fits failed to converge.
    This is a fix for lifetime_regions.

--- lif/lifetime_regions.m
+++ lif/lifetime_regions.m
@@ -12,13 +12,13 @@ lifetime_old = lifetime;
 lifetime = struct([]);
 for x = lifetime_old;
 	x.in_center = img_intensity(x.img, mask_center) ./ x.acc;
-	x.fit_center = fit_decay(x.t, x.in_center);
+	x.fit_center = fit_decay(x.t, x.in_center, "xmin", "peak", "ymin", 0.1);
 	x.tau_center = x.fit_center.fite.tau;
 	x.in_edge1 = img_intensity(x.img, mask_edge1) ./ x.acc;
-	x.fit_edge1 = fit_decay(x.t, x.in_edge1);
+	x.fit_edge1 = fit_decay(x.t, x.in_edge1, "xmin", "peak", "ymin", 0.1);
 	x.tau_edge1 = x.fit_edge1.fite.tau;
 	x.in_edge2 = img_intensity(x.img, mask_edge2) ./ x.acc;
-	x.fit_edge2 = fit_decay(x.t, x.in_edge2);
+	x.fit_edge2 = fit_decay(x.t, x.in_edge2, "xmin", "peak", "ymin", 0.1);
 	x.tau_edge2 = x.fit_edge2.fite.tau;
 	lifetime(end+1) = x;
 endfor
