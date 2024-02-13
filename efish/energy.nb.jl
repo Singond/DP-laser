428ce022a6e53dca60bd0bb67fbe6b5e7ecab3f9
Jan Singon Slany <singond@seznam.cz>
2024-02-14 00:52:00 +0100

    Remove background from EFISH signal

--- efish/energy.nb.jl
+++ efish/energy.nb.jl
@@ -39,7 +39,7 @@ begin
 	index = readdlm("data-22-01-24/energie_info.txt", skipstart = 12)
 	index = index[isa.(index[:,1], Number),:]  # Remove commented-out lines
 
-	X = NamedTuple[]
+	Xraw = NamedTuple[]
 	for (power, amp, oscilo) in eachrow(index)
 		powfile = @sprintf("data-22-01-24/powermeter/571777_%02d.txt", power)
 		powdata = readdlm(powfile, skipstart = 36)
@@ -47,7 +47,7 @@ begin
 		oscfile = @sprintf("data-22-01-24/oscilo/energie_napeti/energie%02d.bin", oscilo)
 		U, efish, I, fd, meta = importkeysightbin(oscfile)
 		efish_peak = -minimum(efish[2])  # Extremum of efish signal (absolute value)
-		push!(X, (; U, efish, efish_peak, I, fd, Epulse))
+		push!(Xraw, (; U, efish, efish_peak, I, fd, Epulse))
 	end
 end
 
@@ -57,8 +57,38 @@ with(legend = :bottomright) do
 	xlabel!("t [ns]")
 	ylabel!("efish [a.u.]")
 	xlims!((48.5, 51))
-	title!("Naměřená odezva EFISH pro různé energie pulzu")
-	for x in X
+	title!("Odezva EFISH pro různé energie pulzu\n(naměřená)")
+	for x in Xraw
+		plot!(x.efish[1].*1e9, x.efish[2],
+			label = @sprintf("%.2f mJ", x.Epulse*1e3))
+	end
+	current()
+end
+
+# ╔═╡ 7422b5c2-0c6d-47bd-a1bf-db000847e0f6
+md"""
+Některá data se zdají být posunuta vůči ostatním.
+Zprůměrujeme tedy intenzitu před začátkem pulzu a tuto hodnotu
+v celém odečteme v celém intervalu jako pozadí.
+"""
+
+# ╔═╡ d0d0a9bb-2fd4-4aa5-a9c4-3ebc73c14dab
+Xcorr = map(Xraw) do x
+	m = x.efish[1] .< 49.2e-9
+	background = mean(x.efish[2][m])
+	x.efish[2] .-= background
+	efish_peak = -minimum(x.efish[2])
+	(; x.U, x.efish, efish_peak, x.I, x.fd, x.Epulse)
+end
+
+# ╔═╡ d57a92fa-af3d-4063-afac-96211cc23a25
+with(legend = :bottomright) do
+	plot()
+	xlabel!("t [ns]")
+	ylabel!("efish [a.u.]")
+	xlims!((48.5, 51))
+	title!("Odezva EFISH pro různé energie pulzu\n(bez pozadí)")
+	for x in Xcorr
 		plot!(x.efish[1].*1e9, x.efish[2],
 			label = @sprintf("%.2f mJ", x.Epulse*1e3))
 	end
@@ -71,9 +101,6 @@ md"""
 Pro snazší porovnání tvaru byla každá odezva dělena svou maximální hodnotou.
 Zdá se, že průběh hlavního pulzu odpovídá, nesedí ale střední hodnoty
 normalizovaného signálu.
-
-**Udělat**:
-Zkusit před normalizací odečíst pozadí, snad si průběhy na sebe sednou lépe.
 """
 
 # ╔═╡ 6465f45d-a08f-4444-9fd4-441c4acf6921
@@ -83,12 +110,20 @@ with(legend = :bottomright) do
 	ylabel!("efish [a.u.]")
 	xlims!((48.5, 51))
 	title!("Normalizovaná odezva EFISH")
-	for x in X
+	for x in Xcorr
 		plot!(x.efish[1].*1e9, x.efish[2]./x.efish_peak,
 			label = @sprintf("%.2f mJ", x.Epulse*1e3))
 	end
 	current()
 end
+
+# ╔═╡ a404dd8c-5d13-4a1b-b84d-8d9e6000aa94
+md"""
+Dále pracujme s normalizovanými daty:
+"""
+
+# ╔═╡ 466cea63-bbf4-4334-8257-177f228002b5
+X = Xcorr
 
 # ╔═╡ b575c692-13fe-49d7-b515-c37aa6d8f34f
 md"""
@@ -147,6 +182,8 @@ end
 md"""
 ## Korelace obou parametrů
 Níže je vykreslena vzájemná závislost maxima signálu a jeho integrálu.
+Korelace je velmi vysoká, při dalším zpracování tedy postačí jako referenční
+hodnotu signálu E-FISH brát jeho maximum.
 """
 
 # ╔═╡ bfb0a570-4aa4-461f-a47c-e7d10840799a
@@ -1325,8 +1362,13 @@ version = "1.4.1+1"
 # ╠═f63d5a9c-b85c-4d79-8afc-f974cd63820a
 # ╠═3c6e7861-8a34-44ad-9af4-fd33e0355293
 # ╠═cbedfbb1-35d2-43b7-8003-350a0204a38e
+# ╟─7422b5c2-0c6d-47bd-a1bf-db000847e0f6
+# ╠═d0d0a9bb-2fd4-4aa5-a9c4-3ebc73c14dab
+# ╠═d57a92fa-af3d-4063-afac-96211cc23a25
 # ╟─b2f01251-e63a-469f-9b8a-d1c48728876b
 # ╠═6465f45d-a08f-4444-9fd4-441c4acf6921
+# ╟─a404dd8c-5d13-4a1b-b84d-8d9e6000aa94
+# ╠═466cea63-bbf4-4334-8257-177f228002b5
 # ╟─b575c692-13fe-49d7-b515-c37aa6d8f34f
 # ╟─415ee871-bd26-4e11-bf73-422800b04f47
 # ╠═0a2d69cb-e8e5-4f9d-acbc-4440a5f63f50
