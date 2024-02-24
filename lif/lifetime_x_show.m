70da0970bde881a04cf510b297389ebc44cf2f76
Jan Singon Slany <singond@seznam.cz>
2024-02-24 16:10:49 +0100

    lif: Enable inspecting fits

--- lif/lifetime_x_show.m
+++ lif/lifetime_x_show.m
@@ -20,6 +20,43 @@ zlabel("intensity I_x [a.u.]");
 view(-75, 20);
 
 fig_taux = figure("name", "Lifetime along x");
+title("Lifetime along x");
 errorbar(x.xpos, x.taux, x.tausigx, "d");
 xlim([min(x.xpos) max(x.xpos)]);
 ylim([0 2*quantile(x.taux, 0.99)]);
+xlabel("position x [px]");
+ylabel('lifetime \tau_e [ns]', "interpreter", "tex")
+
+fig_fit = figure("visible", "off");
+fig_logfit = figure("visible", "off");
+
+function inspect_fitx(s, fits, f1, f2, f3)
+	figure(f1);
+	[x, y, btn] = ginput(1);
+	xr = round(x);
+
+	if (xr > max(s.xpos(:)) || xr < min(s.ypos(:)))
+		return;
+	end
+
+	figure(f2, "name", "Fit detail", "visible", "on");
+	hold on;
+	show_fit_decay(s, fits, [], xr);
+	hold off;
+
+	figure(f3, "name", "Fit detail (log)", "visible", "on");
+	set(gca, "yscale", "log");
+	hold on;
+	show_fit_decay(s, fits, [], xr);
+	hold off;
+end
+
+figure(fig_taux);
+uicontrol("parent", fig_taux,
+	"string", "Inspect fit",
+	"position", [10 10 120 30],
+	"callback", @(a,b) inspect_fitx(x, x.fits, fig_taux, fig_fit, fig_logfit));
+uicontrol("parent", fig_taux,
+	"string", "Clear fits",
+	"position", [140 10 120 30],
+	"callback", @(a,b) clear_figs([fig_fit, fig_logfit]));
