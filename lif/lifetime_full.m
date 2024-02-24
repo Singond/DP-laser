598ce53c4314e5f8425c8cdca7cf867f5d485222
Jan Singon Slany <singond@seznam.cz>
2024-02-24 13:18:05 +0100

    lif: Unify fit intervals

--- lif/lifetime_full.m
+++ lif/lifetime_full.m
@@ -11,7 +11,7 @@ x.in = x.img ./ x.acc;
 
 # Fit intensity decay to obtain lifetime tau
 x.fits = fit_decay(x.t, x.in,
-	"dim", 3, "xmin", 7, "xmax", 15, "progress");
+	"dim", 3, "xmin", 8, "xmax", 15, "progress");
 x.tau = arrayfun(@(a) a.fite.tau, x.fits);
 x.tausig = arrayfun(@(a) a.fite.tausig, x.fits);  # uncertainty of tau
 
@@ -21,7 +21,7 @@ x.tau(!x.tauvalid) = NaN;
 
 x.inx = sum(x.in, 1);
 x.fitsx = fit_decay(x.t, x.inx,
-	"dim", 3, "xmin", 0, "xmax", 15, "progress");
+	"dim", 3, "xmin", 8, "xmax", 15, "progress");
 x.taux = arrayfun(@(a) a.fite.tau, x.fitsx);
 x.tausigx = arrayfun(@(a) a.fite.tausig, x.fitsx);
 
