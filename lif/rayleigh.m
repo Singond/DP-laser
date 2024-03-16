f82b1f1d9efee64d41585e0259f3bc7f8732c155
Jan Singon Slany <singond@seznam.cz>
2024-03-16 14:06:14 +0100

    Edit plots of Rayleight scattering

--- lif/rayleigh.m
+++ lif/rayleigh.m
@@ -17,7 +17,7 @@ x = load_iccd("data-2023-01-20/rayleigh4.SPE", "nodark");
 x.amp = 10;
 R(end+1) = x;
 
-R = arrayfun(@img_intensity, R);
+R = arrayfun(@normalize_intensity, R);
 R = arrayfun(@(x) frame_pulse_energy(x, 50), R);
 R = arrayfun(@frame_pulse_energy, R);
 
@@ -25,14 +25,16 @@ Rold = R;
 R = struct([]);
 for x = Rold
 	x.Em = mean(x.E);
-	x.inm = mean(x.in);
-	x.iny = mean(sum(x.img ./ x.acc, 2), 3);
+	x.inm = mean(x.in, 3);
+	x.in_overall = mean(x.inm(:));
+	x.iny = sum(x.inm, 2);
 	R(end+1) = x;
 endfor
 
 Rt = load_iccd("data-2023-01-20/rayleigh_cas.SPE", "nodark");
 Rt.t = 100:0.5:130;
 Rt.amp = 50;
-Rt = img_intensity(Rt);
+Rt = normalize_intensity(Rt);
 Rt = frametimes(Rt, 50);
 Rt = frame_pulse_energy(Rt);
+Rt.iny = squeeze(sum(Rt.in, 2));
--- lif/rayleigh_show.m
+++ lif/rayleigh_show.m
@@ -4,28 +4,39 @@ end
 
 figure(1, "name", "Example image");
 clf;
-imshow(R(1).img(:,:,1), []);
+imshow(R(1).inm, []);
 title("Rayleigh scattering example image");
 
 figure(2, "name", "Overall intensity");
 clf;
-plot([R.Em]*1e6, [R.inm], "d");
+plot([R.Em]*1e6, [R.in_overall], "d");
 title("Rayleigh scattering overall intensity");
 xlabel("laser power E [\\muJ]");
 ylabel("intensity I [a.u.]");
 
 figure(3, "name", "Time evolution");
 clf;
-plot(Rt.t, Rt.in);
+hold on;
+set(gca, "colororder", winter);
+set(gca, "colororder", lines(length(Rt.t)));
+plot3(
+	Rt.t(ones(size(Rt.ypos)),:),
+	Rt.ypos(:,ones(size(Rt.t))),
+	Rt.iny, "-");
+hold off;
 title("Rayleigh scattering time evolution");
+view(3);
 xlabel("time t [ns]");
-ylabel("intensity I [a.u.]");
+ylabel("position y [px]");
+zlabel("intensity I [a.u.]");
+axis ij;
+grid on;
 
 figure(4, "name", "Beam profile");
 clf;
 hold on;
 for r = R
-	plot(r.iny, "d", "displayname", sprintf("L = %.3g \\muJ", r.Em*1e6));
+	plot(r.iny, "-", "displayname", sprintf("L = %.3g \\muJ", r.Em*1e6));
 end
 hold off;
 title("Beam vertical profile");
