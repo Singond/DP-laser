8524b81f48214812040b1a9dc49f5dd45f9cda86
Jan Singon Slany <singond@seznam.cz>
2024-03-21 23:11:37 +0100

    lif: Fit overall saturation

--- lif/saturation_overall.m
+++ lif/saturation_overall.m
@@ -1,3 +1,40 @@
 saturation_base;
 
 saturation_separate = arrayfun(@img_intensity, saturation_separate);
+
+x = img_intensity(saturation);
+x.lif = x.in;
+
+p = polyfit(x.E, x.lif, logical([1 1 0]));
+x.fitl.a = p(2);
+x.fitl.b = -p(1) * (3 / 2) ./ x.fitl.a;
+x.fitl.f = @(L) polyval(p(:), L);
+
+function F = model_exp(Ly, p)
+	F = p(1) - (p(1)/p(2)) * log(1 + p(2) * Ly) ./ Ly;
+end
+
+s = struct;
+s.bounds = [0 Inf; 0 Inf];
+p0 = [2 * max(x.fitl.a ./ x.fitl.b, 0) max(x.fitl.b, 0)];
+try
+	[~, p, cvg, iter] = leasqr(x.E, x.lif, p0,
+		@(L, p) model_exp(L, p),
+		[], 30, [], [], [], s);
+	if (!cvg)
+		warning(
+			"saturation_overall: Convergence not reached after %d iterations.",
+			iter);
+	end
+catch err
+	warning("dp-laser:fit-failed", ...
+		"fit_saturation: Fit failed: %s", err.message);
+	p = p0;
+end
+
+x.fite.a = p(1) .* p(2) ./ 2;
+x.fite.b = p(2);
+x.fite.iter = iter;
+x.fite.f = @(L) model_exp(L, p);
+
+saturationt = x;
--- lif/saturation_overall_show.m
+++ lif/saturation_overall_show.m
@@ -1,13 +1,29 @@
 saturation_overall;
 
 Escale = 1e6;
-k = 1;
+
 figure();
 hold on;
 for x = saturation_separate;
 	plot(x.E * Escale, x.in, "-d");
 end
 hold off;
-title("Intenzita LIF (integrál celého snímku)");
-xlabel("energie E [\\mu{}J]");
-ylabel("intenzita LIF F [a.u.]");
+title("Integral LIF intensity (data only)");
+xlabel("energy E [\\mu{}J]");
+ylabel("LIF intensity F [a.u.]");
+
+figure();
+x = saturationt;
+[Lmin, Lmax] = bounds(x.E);
+LL = linspace(0, Lmax);
+c = get(gca, "colororder")(get(gca, "colororderindex"),:);
+plot(x.E * Escale, x.lif(:), "d",
+		"color", c, "displayname", "data",
+	LL * Escale, x.fitl.f(LL),
+		"b:", "color", c, "displayname", "polynomial fit",
+	LL * Escale, x.fite.f(LL),
+		"b--", "color", c, "displayname", "general fit");
+title(sprintf("Integral LIF intensity, fitted \\beta = %g", x.fite.b));
+xlabel("energy E [\\mu{}J]");
+ylabel("LIF intensity F [a.u.]");
+legend location northwest;
