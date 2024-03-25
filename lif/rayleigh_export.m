44cf4b0c8968a9e8c9ecb0f011ba2230ab87cc67
Jan Singon Slany <singond@seznam.cz>
2024-03-26 00:09:29 +0100

    lif: Export Rayleigh time evolution plot

--- lif/rayleigh_export.m
+++ lif/rayleigh_export.m
@@ -20,3 +20,24 @@ gp.xlabel('svislá poloha $\\ypos\\,[\\si\\pixel]$');
 gp.ylabel('intenzita LIF $\\lif\\,[\\si\\arbunit]$');
 gp.export("results/rayleigh-profile.tex", "cairolatex", "size 12cm,8cm");
 clear gp;
+
+gp = gnuplotter;
+gp.load("../style.gp");
+gp.load("../style-cairo.gp");
+gp.exec("\n\
+	set key top left \n\
+	set autoscale noextend \n\
+	set offsets graph 0.04, graph 0.04, graph 0.04, graph 0.04 \n\
+	set terminal cairolatex size 12cm,8cm \n\
+	set output 'results/rayleigh-time.tex' \n\
+");
+gp.xlabel('čas $\\tim\\,[\\si\\second]$');
+gp.ylabel('svislá poloha $\\ypos\\,[\\si\\pixel]$');
+gp.exec('set zlabel "intenzita LIF $\\lif\\,[\\si\\arbunit]$"');
+gp.exec("splot '-' with lines");
+for k = 1:rows(Rt.t')
+	D = [Rt.t(k)(ones(size(Rt.ypos))) Rt.ypos Rt.iny(:,k)];
+	gp.data(D, "\n\n");
+end
+gp.exec("e");
+clear gp;
