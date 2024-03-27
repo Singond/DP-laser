91c2594b9ccb5d07e8ff45aede1828476611dccb
Jan Slany <jan.slany@mail.muni.cz>
2024-03-27 16:15:29 +0100

    Document function

--- octave/correct_iccd.m
+++ octave/correct_iccd.m
@@ -1,3 +1,11 @@
+## -*- texinfo -*-
+## @deftypefn {} {@var{S} =} correct_iccd (@var{S})
+##
+## Correct image data in struct @var{S}.
+##
+## Modify @code{@var{S}.img} by subtracting the dark image
+## @code{@var{S}.dark} and clipping negative values to 0.
+## @end deftypefn
 function x = correct_iccd(x)
 	## TODO: Eliminate outliers
 
