58f138ef26d2b96635b606621d59e4d6840e1485
Jan Singon Slany <singond@seznam.cz>
2024-03-29 20:31:42 +0100

    lif: Merge time evolution images to one

--- lif/timeev_export.m
+++ lif/timeev_export.m
@@ -17,23 +17,46 @@ gp.load("gnuplot/rule-lif.gp");
 gp.exec(sprintf("set cbrange [%g:%g]", imin, imax));
 gp.exec("\n\
 	set yrange [:] reverse \n\
-	set xtics 40 \n\
-	set ytics 40 \n\
-	set xlabel '$x$ [\\si\\pixel]' \n\
-	set ylabel '$y$ [\\si\\pixel]' \n\
+	set terminal cairolatex pdf colortext size 12.5cm,17cm \n\
+	set output 'results/timeev.tex' \n\
+	set multiplot layout 3,2 \n\
+	unset key \n\
 	unset colorbox \n\
-	set terminal cairolatex pdf colortext size 8cm,6cm \n\
 ");
-for k = [10:14 15:2:19 21:4:29]
-	name = sprintf("results/timeev-%.1f.tex", t(k));
-	printf("Writing %s\n", name);
-	gp.exec(["set output '" name "'"]);
+row = 1;
+col = 1;
+colpos = [0.14 0.56; 0.58 1.0];
+rowpos = linspace(1, 0.12, 5)' - [0 0.22];
+for k = [11:13 15:2:19 21 25]
+	gp.exec("set tmargin at screen %f", rowpos(row, 1));
+	gp.exec("set bmargin at screen %f", rowpos(row, 2));
+	gp.exec("set lmargin at screen %f", colpos(col, 1));
+	gp.exec("set rmargin at screen %f", colpos(col, 2));
+	if (row == 4)
+		gp.exec("set xtics out nomirror 40");
+		gp.exec("set xlabel '$x$ [\\si\\pixel]'");
+	else
+		gp.exec("unset xtics");
+		gp.exec("unset xlabel");
+	end
+	if (col == 1)
+		gp.exec("set ytics out 40");
+		gp.exec("set ylabel '$y$ [\\si\\pixel]'");
+	else
+		gp.exec("unset ytics");
+		gp.exec("unset ylabel");
+	end
 	gp.exec(sprintf(
 		"set label 2 't = %.1f ns' left at first 20,10 front tc 'white' font ',15'",
 		t(k)));
 	gp.exec("plot '-' matrix with image");
 	gp.data(img(:,:,k));
-	gp.exec("unset output");
+	if (row < 4)
+		row += 1;
+	else
+		row = 1;
+		col += 1;
+	end
 end
 clear gp;
 
