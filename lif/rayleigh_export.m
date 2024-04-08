49c0c779cf70f9ab30e5c7c9f869084676127358
Jan Singon Slany <singond@seznam.cz>
2024-04-09 00:23:18 +0200

    lif: Add normalized beam profile to text

--- lif/rayleigh_export.m
+++ lif/rayleigh_export.m
@@ -24,6 +24,7 @@ gp.exec("\n\
 gp.data(R(1).inm);
 clear gp;
 
+## Beam profile
 gp = gnuplotter;
 gp.load("../gnuplot/style.gp");
 gp.load("../gnuplot/style-cairo.gp");
@@ -39,17 +40,38 @@ end
 gp.xlabel('svislá poloha $\\ypos\\,[\\si\\pixel]$');
 gp.ylabel('intenzita LIF $\\lif\\,[\\si\\arbunit]$');
 gp.export("results/rayleigh-profile.tex", "cairolatex", "pdf size 12cm,8cm");
-## Beam profile
+## small version
+gp.exec("\n\
+	set key horizontal tmargin samplen 1 offset 0,1 \n\
+	set margins at screen 0.25, screen 1, screen 0.2, screen 0.75 \n\
+");
+gp.export("results/rayleigh-profile-s.tex", "cairolatex", "pdf size 6cm,7cm");
+## Normalized beam profile
 gp.clearplot;
-gp.exec("set ytics 0.01");
+gp.exec("\n\
+	unset margins \n\
+	set ytics 0.01 \n\
+	set key vertical inside top left width 1 height 1 samplen 4 \n\
+");
+gp.exec("");
 for k = 1:columns(beamprofile);
 	bpr = beamprofile(:,k);
 	L = beamprofile_L(k);
 	gp.plot(beamprofile_ypos, bpr, sprintf(...
 		'w l t "$\\\\SI{%.2f}{\\\\micro\\\\joule}$"', L*1e6));
 end
-gp.ylabel('normalizovaná intenzita $\\enlasery\\,[\\si{\\per\\pixel}]$');
-gp.export("results/rayleigh-profile-norm.tex", "cairolatex", "pdf size 12cm,8cm");
+gp.ylabel('normalizovaná intenzita $[\\si{\\per\\pixel}]$');
+gp.export("results/rayleigh-profile-norm.tex",...
+	"cairolatex", "pdf size 12cm,8cm");
+## small version
+gp.ylabel('norm. int. $[\\si{\\per\\pixel}]$');
+gp.exec("\n\
+	set key outside top \n\
+	set key horizontal tmargin samplen 1 offset 0,1 \n\
+	set margins at screen 0.25, screen 1, screen 0.2, screen 0.75 \n\
+");
+gp.export("results/rayleigh-profile-norm-s.tex",...
+	"cairolatex", "pdf size 6cm,7cm");
 clear gp;
 
 gp = gnuplotter;
