f4c6d0c2002042cac3dc5978403f34b089c16d68
Jan Singon Slany <singond@seznam.cz>
2024-04-10 23:29:33 +0200

    lif: Improve x-resolved saturation plot

--- lif/saturation_x_export.m
+++ lif/saturation_x_export.m
@@ -12,10 +12,17 @@ gp = gnuplotter;
 gp.load("../gnuplot/style.gp");
 gp.load("../gnuplot/style-cairo.gp");
 ascale = 1e-9;
-gp.title('Proportionality parameter \alpha');
-gp.xlabel("horizontal position x [px]");
-gp.ylabel(sprintf("proportionality parameter \\alpha [10^{%d}]", -log10(ascale)));
-gp.plot(x.xpos, x.fitex.a * ascale, "w p ps 0.5");
-gp.plot(x.xpos, x.fitex.b * ascale, "w p ps 0.5 axes x1y2");
-gp.export("results/saturation-x-params.tex", "cairolatex", "pdf size 12cm,8cm");
+bscale = 1e-6;
+gp.xlabel('poloha $\\ypos\\,[\\si\\pixel]$');
+gp.ylabel('fluorescenční zesílení $\\lifslope\\,[10^9]$');
+gp.exec("\n\
+	set ytics in nomirror \n\
+	set y2tics in nomirror \n\
+	set y2label 'saturační parametr $\\lifsat\\,[\\si{\\per\\micro\\joule}]$' \n\
+	set key top left Left reverse samplen 2 width 1 \n\
+");
+gp.plot(x.xpos, x.fitex.a * ascale, "w p ps 0.4 t '$\\lifslope$'");
+gp.plot(x.xpos, x.fitex.b * bscale, "w p ps 0.5 axes x1y2 t '$\\lifsat$'");
+gp.export("results/saturation-x-params.tex",...
+	"cairolatex", "pdf colourtext size 12cm,8cm");
 clear gp;
