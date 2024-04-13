c8ed192db49bb56ccbdfb88d43d549ce4313bb5c
Jan Singon Slany <singond@seznam.cz>
2024-04-13 17:24:50 +0200

    lif: Add dependency checks to lifetime

--- lif/lifetime_x_show.m
+++ lif/lifetime_x_show.m
@@ -4,6 +4,9 @@ addpath octave;
 if (!exist("lifetime", "var"))
 	if (isfile("results/lifetime.bin"))
 		load results/lifetime.bin
+		if (isolderthan("results/lifetime.bin", "lifetime_full.m"))
+			warning("loading lifetime data from file older than script");
+		end
 	else
 		lifetime_full;
 	end
