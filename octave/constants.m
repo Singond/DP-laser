8e477359d8537a3c7dd97cc01cfd9b53fde61d5b
Jan Singon Slany <singond@seznam.cz>
2024-04-16 00:02:28 +0200

    lif: Make physical constants global

--- octave/constants.m
+++ octave/constants.m
@@ -1,5 +1,5 @@
 pkg load miscellaneous;
 
-lightspeed = physical_constant("speed of light in vacuum");  # [m/s]
-planck = physical_constant("Planck constant");               # [Js]
-boltzmann = physical_constant("Boltzmann constant");         # [J/K]
+global lightspeed = physical_constant("speed of light in vacuum");  # [m/s]
+global planck = physical_constant("Planck constant");               # [Js]
+global boltzmann = physical_constant("Boltzmann constant");         # [J/K]
