ec35fd9678b76b548ff32639feb546782685cf2c
Jan Singon Slany <singond@seznam.cz>
2024-04-21 17:13:15 +0200

    lif: Plot gas compositions into one multiplot

--- lif/concentration_vertical_export.m
+++ lif/concentration_vertical_export.m
@@ -53,4 +53,46 @@ for x = vertical(2:4)
 	gp.clearplot();
 end
 
+disp("Exporting results/concentration-vertical-compositions.tex...");
+gp.exec("\n\
+	unset output \n\
+	set terminal cairolatex pdf colortext size 12.5cm,11.5cm \n\
+	set output 'results/concentration-vertical-compositions.tex' \n\
+	set multiplot layout 2,2 margins 0.1, 0.92, 0.12, 0.98 spacing 0.07, 0.12 \n\
+	set title offset 0,-0.5 \n\
+	set xrange [-11:11] \n\
+	set yrange [-1:16] \n\
+	set xtics 5 \n\
+	set ytics 5 \n\
+	unset xlabel \n\
+	set ylabel '$\\ymm\\,[\\si{\\milli\\metre}]$' \n\
+	unset colorbox \n\
+");
+k = 1;
+for x = vertical(2:4);
+	if (mod(k, 2) == 1)
+		gp.exec("set ytics out nomirror");
+		gp.exec("set ylabel '$\\ymm\\,[\\si{\\milli\\metre}]$'");
+	else
+		gp.exec("unset ytics");
+		gp.exec("unset ylabel");
+	end
+	if (k == 2)
+		gp.exec("set xlabel '$\\xmm\\,[\\si{\\milli\\metre}]$'");
+	elseif (k == 3)
+		gp.exec("set colorbox user origin graph 1.19, 0 size graph 0.1, 1.0");
+		gp.exec(...
+			'set cblabel "$\\ndensse\\,[10^{%d}\\si{\\per\\metre\\cubed}]$" offset 8,0',...
+			-log10(nscale));
+		gp.exec("set cblabel norotate");
+	end
+	gp.exec(...
+		"set title '$\\SI{%d}{\\sccm}\\,\\ce{Ar} + \\SI{%d}{\\sccm}\\,\\ce{H2}$'",...
+		x.sccmAr, x.sccmH2);
+	gp.plotmatrix(x.xmm, x.ys, x.n * nscale, "with image");
+	gp.doplot();
+	gp.clearplot();
+	k++;
+end
+
 clear gp;
