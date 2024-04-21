6410174cbdeb75066acaed267bef246e9d02303c
Jan Singon Slany <singond@seznam.cz>
2024-04-21 16:09:34 +0200

    lif: Show all compositions in script

--- lif/concentration_vertical_show.m
+++ lif/concentration_vertical_show.m
@@ -2,13 +2,13 @@ if (!exist("vertical", "var") || !isfield(vertical, "ins"))
 	concentration_vertical;
 end
 
-
 nscale = 1;
+cmap = viridis;
 
 figure("name", "Concentration in whole flame");
 x = vertical(2);
 maxn = quantile(x.n(:), 0.999);
-imshow(x.n * nscale, [0 maxn] * nscale, "colormap", viridis,
+imshow(x.n * nscale, [0 maxn] * nscale, "colormap", cmap,
 	"xdata", x.xmm, "ydata", x.ys);
 set(gca, "ydir", "normal");
 title("Se concentration in whole flame\n700 sccm Ar + 300 sccm H_2");
@@ -21,10 +21,23 @@ colorbar;
 figure("name", "Concentration in whole flame");
 x = vertical(3);
 maxn = quantile(x.n(:), 0.999);
-imshow(x.n * nscale, [0 maxn] * nscale, "colormap", viridis,
+imshow(x.n * nscale, [0 maxn] * nscale, "colormap", cmap,
 	"xdata", x.xmm, "ydata", x.ys);
 set(gca, "ydir", "normal");
 title("Se concentration in whole flame\n175 sccm Ar + 150 sccm H_2");
+xlabel("position x [mm]");
+ylabel("position y [mm]");
+axis on;
+grid off;
+colorbar;
+
+figure("name", "Concentration in whole flame");
+x = vertical(4);
+maxn = quantile(x.n(:), 0.999);
+imshow(x.n * nscale, [0 maxn] * nscale, "colormap", cmap,
+	"xdata", x.xmm, "ydata", x.ys);
+set(gca, "ydir", "normal");
+title("Se concentration in whole flame\n175 sccm Ar + 50 sccm H_2");
 xlabel("position x [mm]");
 ylabel("position y [mm]");
 axis on;
