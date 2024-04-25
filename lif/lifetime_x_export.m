684af555e445fdd96ea9712767ae30e343f8ef5c
Jan Singon Slany <singond@seznam.cz>
2024-04-25 02:49:26 +0200

    lif: Plot x-resolved signal evolution in time

--- lif/lifetime_x_export.m
+++ lif/lifetime_x_export.m
@@ -64,3 +64,44 @@ end
 gp.export("results/lifetime-x-fits.tex",...
 	"cairolatex", "pdf colourtext size 12cm,8cm");
 clear gp;
+
+##
+## Time evolution
+##
+gp = gnuplotter;
+gp.load("../gnuplot/style.gp");
+gp.load("../gnuplot/style-cairo.gp");
+gp.load("../gnuplot/style-splot.gp");
+gp.exec("\n\
+	set style fill transparent solid 0.3 \n\
+	set tmargin at screen 0.82 \n\
+	set bmargin at screen 0.25 \n\
+	set lmargin at screen 0.24 \n\
+	set rmargin at screen 0.85 \n\
+	set yrange noextend \n\
+	set ytics 20 \n\
+	set ztics 400 \n\
+	set xyplane at 0 \n\
+	set autoscale noextend \n\
+	#set view 60, 360-37.5 \n\
+	unset key \n\
+	set offsets graph 0.04, graph 0.04, graph 0.04, graph 0.04 \n\
+	set terminal cairolatex pdf size 12.5cm,10cm \n\
+	set output 'results/lifetime-x-time.tex' \n\
+");
+gp.exec('set xlabel "čas $\\tim\\,[\\si{\\nano\\second}]$" offset -1,-0.5 rotate parallel');
+gp.exec('set ylabel "poloha $\\xpos\\,[\\si\\pixel]$" offset -1,0 rotate parallel');
+gp.exec('set zlabel "intenzita LIF $\\lif\\,[\\si\\arbunit]$" offset -1,0');
+gp.exec("splot '-' with zerrorfill ls 1, '-' with lines ls 1");
+sz = size(x.xpos');
+for k = 1:numel(x.t)
+	D = [x.t(k)(ones(sz)) x.xpos' x.inx(:,:,k)' zeros(sz) x.inx(:,:,k)'];
+	gp.data(D, "\n\n");
+end
+gp.exec("e");
+for k = 1:numel(x.t)
+	D = [x.t(k)(ones(sz)) x.xpos' x.inx(:,:,k)'];
+	gp.data(D, "\n\n");
+end
+gp.exec("e");
+clear gp;
