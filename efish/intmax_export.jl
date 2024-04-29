1b41c255e2061e37e35466fb0274b17f32680230
Jan Singon Slany <singond@seznam.cz>
2024-04-29 23:34:12 +0200

    efish: Fix plot range after data rescaling

--- efish/intmax_export.jl
+++ efish/intmax_export.jl
@@ -13,8 +13,7 @@ end
 @gp """
 	load '../gnuplot/style.gp'
 	load '../gnuplot/style-cairo.gp'
-	set xrange [-0.2:6.2]
-	set yrange [-0.05:1.25]
+	set yrange [0:0.31]
 	set xlabel '\$max(\\efish)\\,[\\si{\\arbunit}]\$'
 	set ylabel '\$\\int\\efish\\dd{t}\\,[\\si{\\arbunit}]\$' #offset -0.5,0
 	unset key
