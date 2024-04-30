ef5c3e8d04679ab2508e6073ff916abf0fcbd445
Jan Slany <jan.slany@mail.muni.cz>
2024-04-30 16:46:12 +0200

    talif: Display fits in plot

--- talif/show_lifetime.m
+++ talif/show_lifetime.m
@@ -30,9 +30,21 @@ title("Time evolution of intensity (normalized)");
 xlabel("time t [ns]");
 ylabel("intensity [a.u.]");
 hold on;
+k = 1;
 for x = X
-	plot(x.t, x.in ./ max(x.in),
+	c = max(x.in);
+	set(gca, "colororderindex", k);
+	plot(x.t, x.in ./ c,
 		"displayname", sprintf("%d Pa", x.p1));
+
+	tt = linspace(min(x.t), max(x.t), 1000);
+	set(gca, "colororderindex", k);
+	plot(tt, x.fite.f(tt) ./ c, "--", "handlevisibility", "off");
+
+	set(gca, "colororderindex", k);
+	plot(tt, x.fitb.f(tt) ./ c, ":", "handlevisibility", "off");
+
+	k++;
 end
 hold off;
 legend location northeast;
