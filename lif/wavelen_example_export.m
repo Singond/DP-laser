56cdb3ec94b8931058c4750e1dd4cfcb1e2e9275
Jan Singon Slany <singond@seznam.cz>
2024-06-12 02:12:26 +0200

    present: Add dark images statistics

--- lif/wavelen_example_export.m
+++ lif/wavelen_example_export.m
@@ -2,6 +2,8 @@ pkg load report;
 
 wavelen_main;
 
+## Energy variation
+
 gp = gnuplotter;
 gp.load("../gnuplot/style.gp");
 gp.load("../gnuplot/style-cairo.gp");
@@ -26,4 +28,71 @@ Estd = std(edata(:,2));
 printf("mean pulse energy:      %f uJ\n", Em * 1e6);
 printf("pulse energy st. dev:   %f uJ (%f %%)\n",...
 	Estd * 1e6, 100 * Estd / Em);
+disp("");
 
+## Dark frame variation
+printf("Single series\n");
+W1 = load_iccd("data-2023-01-20/scan9.SPE");
+W1.imgs = squeeze(mean(mean(W1.img, 1), 2));
+[W1.imgsmax, imax] = max(W1.imgs);
+W1.darks = squeeze(mean(mean(W1.dark, 1), 2));
+W1.darksmean = mean(W1.darks);
+W1.darksstd = std(W1.darks);
+printf("max img value:          %f\n", W1.imgsmax);
+printf("mean dark value:        %f (%f %%)\n",...
+	W1.darksmean, 100 * W1.darksmean / W1.imgsmax);
+printf("dark std. deviation:    %f (%f %%)\n",...
+	W1.darksstd, 100 * W1.darksstd / W1.darksmean);
+disp("");
+
+printf("Single frame (#%d)\n", imax);
+w = W1.img(:,:,imax);
+wd = mean(W1.dark, 3);
+printf("max img value:          %f\n", max(w(:)));
+printf("q95 img value:          %f\n", quantile(w(:), 0.95));
+printf("mean img value:         %f\n", mean(w(:)));
+printf("mean dark value:        %f\n", mean(wd(:)));
+disp("");
+
+printf("Multiple series (each averaged)\n");
+rawimgs = cat(4, W.img) + cat(4, W.dark);
+rawimgs = squeeze(mean(rawimgs, 3));
+imgs = squeeze(mean(mean(rawimgs, 1), 2));
+imgsmax = max(imgs);
+darks = squeeze(mean(mean(cat(3, W.dark), 1), 2));
+darksmean = mean(darks);
+darksstd = std(darks);
+printf("max img value:          %f\n", imgsmax);
+printf("mean dark value:        %f (%f %%)\n",...
+	darksmean, 100 * darksmean / imgsmax);
+printf("dark std. deviation:    %f (%f %%)\n",...
+	darksstd, 100 * darksstd / darksmean);
+disp("");
+
+gp = gnuplotter;
+gp.load("../gnuplot/style.gp");
+gp.load("gnuplot/style-lif.gp");
+gp.exec("\n\
+	set tmargin at screen 1 \n\
+	set rmargin at screen 1 \n\
+	set bmargin at screen 0 \n\
+	set lmargin at screen 0 \n\
+	set yrange [:] reverse \n\
+	set cbrange [0:2500] \n\
+	unset xtics \n\
+	unset ytics \n\
+	unset key \n\
+	unset colorbox \n\
+");
+opts = "pdf size 3.15cm,2.7cm";
+r = 50:110;
+c = 60:130;
+gp.plotmatrix(w(r,c), "with image");
+gp.export("results/example-img-corr-small.tex", "cairolatex", opts);
+gp.clearplot;
+gp.plotmatrix(wd(r,c), "with image");
+gp.export("results/example-img-dark-small.tex", "cairolatex", opts);
+gp.clearplot;
+gp.plotmatrix(w(r,c) + wd(r,c), "with image");
+gp.export("results/example-img-raw-small.tex", "cairolatex", opts);
+clear gp;
